4bfa1916ebe7272fb217ec8af656977ee42652b3
haritiana adriano <hei.adriano.4@gmail.com>
2023-01-23 17:39:07 +0300

    feat: TODO1 player_scorer_to_entity_ok

--- src/test/java/PlayerMapperTest.java
+++ src/test/java/PlayerMapperTest.java
@@ -68,6 +68,43 @@ public class PlayerMapperTest {
 
     @Test
     void player_scorer_to_entity_ok() {
-
+        PlayerScoreEntity expect = PlayerScoreEntity.builder()
+                .match(MatchEntity.builder()
+                        .scorers(new ArrayList<>())
+                        .teamA(TeamEntity.builder()
+                                .id(1)
+                                .name("T1")
+                                .build())
+                        .teamB(TeamEntity.builder()
+                                .name("T2")
+                                .id(2)
+                                .build())
+                        .stadium("Stade-1")
+                        .id(1)
+                        .datetime(Instant.now())
+                        .build())
+                .player(PlayerEntity.builder()
+                        .team(TeamEntity.builder()
+                                .id(1)
+                                .name("T1")
+                                .build())
+                        .guardian(false)
+                        .name("string")
+                        .build())
+                .ownGoal(false)
+                .minute(12)
+                .build();
+        PlayerScorer actual = PlayerScorer.builder()
+                .player(Player.builder()
+                        .id(1)
+                        .name("string")
+                        .teamName("T1")
+                        .isGuardian(false)
+                        .build())
+                .isOwnGoal(false)
+                .minute(12)
+                .build();
+        when(subject.toEntity(1, actual)).thenReturn(expect);
+        assertEquals(expect, subject.toEntity(1, actual));
     }
 }
