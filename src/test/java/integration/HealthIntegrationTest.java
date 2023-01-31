c5852604212ebd20ce379bb37d7edcd8b8124a22
haritiana adriano <hei.adriano.4@gmail.com>
2023-01-31 07:29:45 +0300

    feat: health integration test

---
+++ src/test/java/integration/HealthIntegrationTest.java
@@ -0,0 +1,33 @@
+package integration;
+
+import app.foot.FootApi;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = FootApi.class)
+@AutoConfigureMockMvc
+public class HealthIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void health_ok() throws Exception{
+        MockHttpServletResponse response = mockMvc.perform(get("/ping"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+}
